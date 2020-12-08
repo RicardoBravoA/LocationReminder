2f7694ca14348b8f1d1437a3d712e9004ec9a88d
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-08 17:35:01 -0500

    feat: Add geofence broadcast receiver

---
+++ app/src/main/java/com/udacity/location/reminder/geofence/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.udacity.location.reminder.geofence
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+
+//TODO: implement the onReceive method to receive the geofencing events at the background
+
+    }
+}
