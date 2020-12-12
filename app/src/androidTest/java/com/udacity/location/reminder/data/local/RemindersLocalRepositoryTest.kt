e968b9744762ae8120939d2981e0b994cfe9ba7b
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-12 18:16:51 -0500

    feat(RemindersLocalRepositoryTest): Add setup function

--- app/src/androidTest/java/com/udacity/location/reminder/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/location/reminder/data/local/RemindersLocalRepositoryTest.kt
@@ -1,9 +1,13 @@
 package com.udacity.location.reminder.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 
@@ -17,5 +21,20 @@ class RemindersLocalRepositoryTest {
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries()
+            .build()
+
+        remindersLocalRepository =
+            RemindersLocalRepository(
+                database.reminderDao(),
+                Dispatchers.Main
+            )
+    }
 
 }
