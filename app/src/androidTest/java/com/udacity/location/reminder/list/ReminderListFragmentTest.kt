128ddca6c519724a7d4a2bcad8db2713f93e1ecb
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-12 22:28:38 -0500

    feat: Validate recyclerview size

--- app/src/androidTest/java/com/udacity/location/reminder/list/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/location/reminder/list/ReminderListFragmentTest.kt
@@ -1,6 +1,7 @@
 package com.udacity.location.reminder.list
 
 import android.os.Bundle
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -12,35 +13,61 @@ import com.udacity.location.reminder.R
 import com.udacity.location.reminder.data.ReminderDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Test
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.action.ViewActions.click
+import com.udacity.location.reminder.data.dto.ReminderEntity
+import org.junit.Before
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
 import org.koin.test.KoinTest
 import org.koin.test.inject
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
 @ExperimentalCoroutinesApi
 class ReminderListFragmentTest : KoinTest {
 
-    private val reminderDataSource: ReminderDataSource by inject()
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 
-    @Test
-    fun clickAddButton_navigateToMapLocationFragment() {
+    private val reminderDataSource: ReminderDataSource by inject()
+    private val reminderEntity =
+        ReminderEntity("Title", "description", "Googleplex", 40.7536, -73.9831, 100)
+    private lateinit var navController: NavController
+
+    @Before
+    fun setup() {
         val scenario = launchFragmentInContainer<ReminderListFragment>(
             Bundle(),
             R.style.Theme_LocationReminder
         )
-        val navController = mock(NavController::class.java)
+        navController = mock(NavController::class.java)
         scenario.onFragment {
             Navigation.setViewNavController(it.view!!, navController)
         }
+    }
 
+    @Test
+    fun clickAddButton_navigateToMapLocationFragment() {
         onView(ViewMatchers.withId(R.id.add_button)).perform(click())
-
         verify(navController).navigate(
             ReminderListFragmentDirections.toSaveReminder()
+        )
+    }
+
+    @Test
+    fun validateDataInRecyclerView() {
+        runBlocking {
+            reminderDataSource.saveReminder(reminderEntity)
+        }
+
+        onView(ViewMatchers.withId(R.id.reminder_recycler_view)).check(
+            matches(
+                hasChildCount(1)
+            )
         )
     }
 
