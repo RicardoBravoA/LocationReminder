97ccdf7a490463a40ef77bce4b31a6fa87f4f148
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-12 19:36:07 -0500

    feat: Add app navigation test class

---
+++ app/src/androidTest/java/com/udacity/location/reminder/AppNavigationTest.kt
@@ -0,0 +1,11 @@
+package com.udacity.location.reminder
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AppNavigationTest {
+
+}
